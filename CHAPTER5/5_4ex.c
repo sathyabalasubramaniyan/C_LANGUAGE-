c4285a80440e3a92d41e99775fcb00f082ecb0a5
SATHYA B <72013783+sathyabalasubramaniyan@users.noreply.github.com>
2022-02-24 17:38:21 +0530

    Update 5_4ex.c

--- CHAPTER5/5_4ex.c
+++ CHAPTER5/5_4ex.c
@@ -1,4 +1,4 @@
-Write the function strend(s,t) , which returns 1 if the string t occurs at the end of the string s , and zero otherwise.
+//Write the function strend(s,t) , which returns 1 if the string t occurs at the end of the string s , and zero otherwise.
 
 #include<stdio.h>
 
