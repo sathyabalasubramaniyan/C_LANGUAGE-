8926667e8081eeea76da6cacfd57d4af255924bf
SATHYA B <72013783+sathyabalasubramaniyan@users.noreply.github.com>
2022-02-24 18:09:54 +0530

    Update 5_5ex.c

--- CHAPTER5/5_5ex.c
+++ CHAPTER5/5_5ex.c
@@ -1,3 +1,4 @@
+//Write versions of the library functions strncpy , strncat , and strncmp , which operate on at most the first n characters of their argument strings. For example, strncpy(s,t,n) copies at most n characters of t to s 
 #include<stdio.h>
 
 //macro
